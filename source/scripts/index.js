29f4d1857b500223bad7d4f76f068a0c36520183
IgorSamoylov <gronysrg@gmail.com>
2024-10-21 18:39:52 +0300

    Исправляет все недочеты

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,10 +1,11 @@
 /* в этот файл добавляет скрипты*/
 window.addEventListener('load', () => {
   const burger = document.querySelector('.js-burger');
-  const navigation = document.querySelector('.js-navigation');
+  const navigation = document.querySelector('.js-navigation', '.main-nav--no-js');
 
   burger.classList.remove('is-active');
   navigation.classList.remove('is-show');
+  navigation.classList.remove('main-nav--no-js');
 
   burger.addEventListener('click', (e) => {
     e.preventDefault();
